ef2198964eabedce8f4b54362ee43631373348a9
Kai Davenport <kaiyadavenport@gmail.com>
2014-07-09 15:53:08 +0000

    padding / sizing

--- index.js
+++ index.js
@@ -3,13 +3,13 @@ var percent = require('percent-string')
 module.exports = BookBinding;
 
 var defaults = {
-	image:'build/binocarlos-bookbinding/bookbg.png',
-	padding:{
-		left:10,
-		right:10,
-		top:10,
-		bottom:10
-	}
+  image:'build/binocarlos-bookbinding/bookbg.png',
+  padding:{
+    left:10,
+    right:10,
+    top:10,
+    bottom:10
+  }
 }
 
 function BookBinding (opts) {
@@ -17,22 +17,28 @@ function BookBinding (opts) {
   var self = this;
   this.opts = opts = opts || {}
   Object.keys(defaults || {}).forEach(function(key){
-  	if(!opts[key]){
-  		opts[key] = defaults[key]
-  	}
+    if(!opts[key]){
+      opts[key] = defaults[key]
+    }
   })
   Object.keys(opts || {}).forEach(function(key){
-  	if(key.indexOf('padding')==0){
-  		var val = opts[key]
-  		key = key.replace(/^padding/, '').toLowerCase()
-  		opts.padding[key] = val
-  	}
+    if(key.indexOf('padding')==0){
+      var val = opts[key]
+      key = key.replace(/^padding/, '').toLowerCase()
+      opts.padding[key] = val
+    }
   })
   this.element = document.createElement('div')
   this.element.style.backgroundImage = 'url(' + this.opts.image + ')'
+  this.element.style.backgroundSize = 'contain'
+  this.element.style.backgroundRepeat = 'no-repeat'
+  this.element.style.backgroundPosition = 'center top'
+  this.element.style.width = '100%'
+  this.element.style.height = '100%'
+  /*
   Object.keys(this.opts.padding || {}).forEach(function(key){
     self.element.style['padding-' + key] = percent(self.opts.padding[key])
-  })
+  })*/
 }
 
 BookBinding.prototype.appendChild = function (child) {

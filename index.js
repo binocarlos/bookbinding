bcd0903f792de1fa345a89e5199fdd7442f2ddd6
Kai Davenport <kaiyadavenport@gmail.com>
2014-07-10 09:06:18 +0000

    larger default size

--- index.js
+++ index.js
@@ -6,10 +6,10 @@ var defaults = {
   image:'build/binocarlos-bookbinding/bookbg.png',
   imageWidth:939,
   imageHeight:570,
-  left:'3.6%',
+  left:'3.5%',
   top:'2.8%',
-  width:'93.3%',
-  height:'93.2%'
+  width:'93.5%',
+  height:'93.4%'
 }
 
 var fields = ['left', 'top', 'width', 'height']
